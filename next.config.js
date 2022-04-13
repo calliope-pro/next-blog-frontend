c157665fc4ec1b41cda5158047df21f2fe6c3978
calliope <calliope183@gmail.com>
2022-04-13 20:21:22 +0900

    update conf eslint

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
   reactStrictMode: true,
   env: {
     ENV_SECURITY_CODE: process.env.ENV_SECURITY_CODE
-  }
+  },
+  images: {
+    domains: [new URL(process.env.NEXT_PUBLIC_BACKEND_ORIGIN).hostname]
+  },
 }

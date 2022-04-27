f735ed5856d659d8c6e039db5ad1130bb215a348
calliope <calliope183@gmail.com>
2022-04-27 22:44:18 +0900

    fix to sync tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,19 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html:
-              '(admaxads = window.admaxads || []).push({admax_id: "6795d28641044c55c462bac5e347344c", type: "action"});',
-          }}
-        />
-        <script
-          type="text/javascript"
-          charSet="utf-8"
-          src="https://adm.shinobi.jp/st/t.js"
-          async
-        />
+        <script src="https://adm.shinobi.jp/s/6795d28641044c55c462bac5e347344c"></script>
       </body>
     </Html>
   );

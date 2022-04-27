5375b67850e808ab114dca5c01da9c59756e099e
calliope <calliope183@gmail.com>
2022-04-27 22:47:27 +0900

    fix build error No Sync Scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,7 +8,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script src="https://adm.shinobi.jp/s/6795d28641044c55c462bac5e347344c"></script>
+        <Script src="https://adm.shinobi.jp/s/6795d28641044c55c462bac5e347344c"></Script>
       </body>
     </Html>
   );

880bab3b7c3a554d0e9e5a466434401c29a73f9f
calliope <calliope183@gmail.com>
2022-04-27 22:50:43 +0900

    fix build error delete `_document`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,24 @@ import '../../styles/global.scss';
 import type { AppProps } from 'next/app';
 
 import Head from 'next/head';
+import Script from 'next/script';
 import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <>
+      <RecoilRoot>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </RecoilRoot>
+      <Script src="https://adm.shinobi.jp/s/6795d28641044c55c462bac5e347344c"></Script>
+    </>
   );
 }
 

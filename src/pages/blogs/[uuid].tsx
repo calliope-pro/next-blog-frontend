bcab21ec48ad7fd2389d44626f5d2509f51a4957
calliope <calliope183@gmail.com>
2022-09-29 03:37:11 +0900

    change to ISR

--- src/pages/blogs/[uuid].tsx
+++ src/pages/blogs/[uuid].tsx
@@ -1,25 +1,35 @@
 import type { Blog } from '#src/types';
-import type { GetServerSideProps, NextPage } from 'next';
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 
-import { BlogDetail } from '#src/components';
+import { BlogDetail, Loader } from '#src/components';
 import { ClientLayout } from '#src/layouts/client';
 import { fetchBlog } from '#src/utils/api/blog';
+import { useRouter } from 'next/router';
 
 const BlogDetailPage: NextPage<{ blog: Blog }> = ({ blog }) => {
+    const router = useRouter();
     return (
         <ClientLayout isAdsExist>
-            <BlogDetail blog={blog} />
+            {router.isFallback ? <Loader /> : <BlogDetail blog={blog} />}
         </ClientLayout>
     );
 };
 
 export default BlogDetailPage;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticPaths: GetStaticPaths = () => {
+    return {
+        paths: [],
+        fallback: true,
+    };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
     try {
-        const blog = await fetchBlog(context.query.uuid as string);
+        const blog = await fetchBlog(context.params?.uuid as string);
         return {
             props: { blog },
+            revalidate: 60,
         };
     } catch (error) {
         return { notFound: true };

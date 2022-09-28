5aaa7103501ea41df077c0eb44749da66f6101a0
calliope <calliope183@gmail.com>
2022-09-29 05:31:00 +0900

    fix color

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,6 @@
 export const COLORS = {
     baseColor: '#232946',
-    mainColor: 'rgb(223, 236, 238)',
+    mainColor: 'rgb(193, 202, 243)',
     accentColor: '#eebbc3',
     accentDarkColor: '#ee7587',
     whiteColor: '#fffffe',

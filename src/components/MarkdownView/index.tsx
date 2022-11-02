4bd3cbda6cfa8e84a6118e5a5d93c3311f2bbdab
calliope <calliope183@gmail.com>
2022-11-02 23:49:02 +0900

    change jsx

--- src/components/MarkdownView/index.tsx
+++ src/components/MarkdownView/index.tsx
@@ -1,12 +1,13 @@
 import style from './md-view.module.scss';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { prism, okaidia } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { useRecoilValue } from 'recoil';
 import remarkGfm from 'remark-gfm';
-import { Box } from '@mui/material';
+import { Box, Link as MuiLink } from '@mui/material';
 
 import { CodeContainer } from './CodeContainer';
 import { isDarkState } from '#src/atoms/codeStyleAtom';
@@ -59,10 +60,10 @@ export const MarkdownView: React.FC<{ body: string }> = ({ body }) => {
                             {String(children)}
                         </Box>
                     ),
-                    img: ({ node }) => (
+                    img: ({ src, alt }) => (
                         <Image
-                            src={node.properties!.src as string}
-                            alt={node.properties!.alt as string}
+                            src={src as string}
+                            alt={alt as string}
                             quality={90}
                             width="100%"
                             height="40%"
@@ -70,6 +71,21 @@ export const MarkdownView: React.FC<{ body: string }> = ({ body }) => {
                             objectFit="contain"
                         />
                     ),
+                    a: ({ children, href }) => {
+                        const isExternalUrl = href?.startsWith('http');
+                        return (
+                            <Link passHref href={href as string}>
+                                <MuiLink
+                                    target={
+                                        isExternalUrl ? '_blank' : undefined
+                                    }
+                                    rel={isExternalUrl ? 'noopener' : undefined}
+                                >
+                                    {children}
+                                </MuiLink>
+                            </Link>
+                        );
+                    },
                 }}
             >
                 {body}

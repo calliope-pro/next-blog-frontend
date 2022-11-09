d97b8b16e6941b5cca2b9b28c9ea54d1503ea454
calliope <calliope183@gmail.com>
2022-11-10 04:05:15 +0900

    fix url

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -23,7 +23,7 @@ export default async function handler(
                           ? '/usr/bin/google-chrome'
                           : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
               };
-        await chromium.font('https://s3.ap-northeast-2.amazonaws.com/fonts/webfont.ttf')
+        await chromium.font('https://fonts.googleapis.com/css?family=Sawarabi+Mincho')
         const browser = await puppeteer.launch(options);
         const page = await browser.newPage();
         await page.goto(url);

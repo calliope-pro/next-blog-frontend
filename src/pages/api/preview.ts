6e6be0d6b357c66ab6436386117ca1d60e44197d
calliope <calliope183@gmail.com>
2022-11-10 01:58:43 +0900

    use puppeteer on vercel

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,5 +1,6 @@
+import chromium from 'chrome-aws-lambda';
 import { NextApiRequest, NextApiResponse } from 'next';
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 
 export default async function handler(
     req: NextApiRequest,
@@ -21,7 +22,11 @@ export default async function handler(
 }
 
 const getImageBase64 = async (url: string) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
     const page = await browser.newPage();
     await page.goto(url);
     const image = await page.screenshot({ encoding: 'base64' });

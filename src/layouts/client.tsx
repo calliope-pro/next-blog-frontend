a12e7d2b0f6d197e73e29a2e6f7d2a2ebea1dbe8
calliope <calliope183@gmail.com>
2023-02-19 03:56:19 +0900

    add auto google ads script

--- src/layouts/client.tsx
+++ src/layouts/client.tsx
@@ -3,7 +3,6 @@ import { Stack } from '@mui/material';
 
 import { Footer, Navbar } from '#src/components';
 import { COLORS } from '#src/styles';
-import Head from 'next/head';
 
 type ClientLayoutPropType = {
     isAdsExist?: boolean;
@@ -11,54 +10,19 @@ type ClientLayoutPropType = {
 
 export const ClientLayout: React.FC<ClientLayoutPropType> = ({
     children,
-    isAdsExist,
 }) => {
-    const AdmaxAd = (
-        <>
-            <div
-                className="admax-ads"
-                data-admax-id="f11b860d747ce3037ea1083fec43a96b"
-                style={{
-                    display: 'inline-block',
-                    width: '728px',
-                    height: '90px',
-                    margin: '0 auto',
-                }}
-            />
-            <Script
-                id="admax-ads"
-                type="text/javascript"
-                dangerouslySetInnerHTML={{
-                    __html: '(admaxads = window.admaxads || []).push({admax_id: "f11b860d747ce3037ea1083fec43a96b",type: "banner"});',
-                }}
-            />
-            <Script
-                type="text/javascript"
-                src="https://adm.shinobi.jp/st/t.js"
-                async
-            />
-        </>
-    );
     return (
         <>
-            <Head>
-                {/* eslint-disable-next-line @next/next/next-script-for-ga */}
-                <script
-                    async
-                    src="https://www.googletagmanager.com/gtag/js?id=G-Y838E4H5WF"
-                />
-                <script
-                    id="Google-tag-gtag-js"
-                    dangerouslySetInnerHTML={{
-                        __html: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-Y838E4H5WF');",
-                    }}
-                />
-            </Head>
+            <Script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5059912395081075"
+                crossOrigin="anonymous"
+                strategy="beforeInteractive"
+            />
 
             <Stack minHeight="100vh" sx={{ bgcolor: COLORS.mainColor }} justifyContent="space-between">
                 <Navbar />
                 {children}
-                {isAdsExist && AdmaxAd}
                 <Footer />
             </Stack>
         </>

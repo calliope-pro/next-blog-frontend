3a62d427f4afe5e26e3c1baf1f150123021a7800
calliope <calliope183@gmail.com>
2023-02-22 18:32:45 +0900

    add useEffect

--- src/layouts/client.tsx
+++ src/layouts/client.tsx
@@ -1,5 +1,6 @@
 import Script from 'next/script';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { Stack } from '@mui/material';
 
 import { Footer, Navbar } from '#src/components';
@@ -7,6 +8,13 @@ import { COLORS } from '#src/styles';
 
 export const ClientLayout: React.FC = ({ children }) => {
     const router = useRouter();
+    const [key, setKey] = useState('');
+
+    useEffect(() => {
+        if (router.isReady) {
+            setKey(router.asPath);
+        }
+    }, [router]);
     return (
         <>
             <Script
@@ -14,7 +22,7 @@ export const ClientLayout: React.FC = ({ children }) => {
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5059912395081075"
                 crossOrigin="anonymous"
                 strategy="beforeInteractive"
-                key={router.asPath}
+                key={key}
             />
 
             <Stack

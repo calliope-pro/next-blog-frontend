252879d91d334fd7a0750d559c94277bf29408b0
calliope <calliope183@gmail.com>
2023-03-04 19:11:09 +0900

    fix url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,23 @@
 import '../../styles/global.scss';
 import type { AppProps } from 'next/app';
 
+import nextAbsoluteUrl from 'next-absolute-url';
 import { DefaultSeo } from 'next-seo';
 import { RecoilRoot } from 'recoil';
 import { useRouter } from 'next/router';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
 
 const theme = createTheme();
 
 function MyApp({ Component, pageProps }: AppProps) {
     const router = useRouter();
+    const [ogImageUrl, setOgImageUrl] = useState('');
+    useEffect(() => {
+        if (router.isReady) {
+            setOgImageUrl(new URL('/api/og', nextAbsoluteUrl().origin).href);
+        }
+    }, [router]);
     return (
         <RecoilRoot>
             <ThemeProvider theme={theme}>
@@ -34,7 +42,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                             {
                                 width: 1200,
                                 height: 630,
-                                url: '/api/og',
+                                url: ogImageUrl,
                                 alt: 'open graph card',
                             },
                         ],

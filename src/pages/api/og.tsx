829ce87b5fe943f4aa5ac3b26a01be71eccd80c8
calliope <calliope183@gmail.com>
2023-03-05 18:14:46 +0900

    fix url

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -45,12 +45,7 @@ export default function handler(req: NextApiRequest) {
                         position: 'absolute',
                         right: 120,
                         bottom: '10%',
-                        backgroundImage: `url(${
-                            new URL(
-                                '/favicon.png',
-                                process.env.NEXT_PUBLIC_FRONTEND_ORIGIN,
-                            ).href
-                        }`,
+                        backgroundImage: `url(${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN!}/favicon.png`,
                         backgroundSize: '100% 100%',
                         height: 60,
                         width: 60,

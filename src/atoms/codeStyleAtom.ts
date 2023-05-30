b7369d245d264e544294dbe91eff4394ab9d5451
calliope <calliope183@gmail.com>
2023-05-30 22:38:42 +0900

    add effect to persist

--- src/atoms/codeStyleAtom.ts
+++ src/atoms/codeStyleAtom.ts
@@ -1,6 +1,20 @@
 import { atom } from 'recoil';
 
+const LocalStorageKey = 'isDark';
 export const isDarkState = atom<boolean>({
     key: 'isDarkState',
-    default: true,
+    default: (() => {
+        if (typeof window === 'undefined') {
+            return true;
+        }
+        const isDark = localStorage.getItem(LocalStorageKey);
+        return !isDark || isDark == 'y';
+    })(),
+    effects: [
+        ({ onSet }) => {
+            onSet((newValue) => {
+                localStorage.setItem(LocalStorageKey, newValue ? 'y' : 'n');
+            });
+        },
+    ],
 });
